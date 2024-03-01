69103918b3f104e1684af8480e7d2719b76bcfb6
Jakub Radzik <jradzik4@gmail.com>
2024-03-01 10:25:32 +0100

    fix

--- src/Screens/ProductDetails.tsx
+++ src/Screens/ProductDetails.tsx
@@ -1,8 +1,49 @@
-import {View} from 'react-native';
-import {ProductDetailsScreenProps} from '../types/routes';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  Animated,
+  Dimensions,
+  Image,
+  PanResponder,
+  StyleSheet,
+  View,
+} from 'react-native';
 
-export const ProductDetailsScreen = ({
-  navigation,
-}: ProductDetailsScreenProps) => {
-  return <View></View>;
+export const ProductDetailsScreen = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const interval = 30;
+  const images = Array.from({length: 55}, (_, i) => {
+    return `https://www.apple.com/105/media/us/airpods-3rd-generation/2021/3c0b27aa-a5fe-4365-a9ae-83c28d10fa21/anim/battery/large_2x/00${i}.jpg`;
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
+    }, interval);
+
+    return () => clearInterval(intervalId);
+  }, [currentIndex]);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: images[currentIndex],
+        }}
+        style={styles.image}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+});
