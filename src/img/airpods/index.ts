71badc82b370b7f91a5304816c4dd2dd46032df1
Jakub Radzik <jradzik4@gmail.com>
2024-03-02 11:52:35 +0100

    images

--- src/img/airpods/index.ts
+++ src/img/airpods/index.ts
@@ -1,10 +1,20 @@
 import {ImageSourcePropType} from 'react-native';
 
 export const airpods: ImageSourcePropType[] = [
-  require('./result-0-1.jpg'),
-  require('./result-1-1.jpg'),
-  require('./result-2-1.jpg'),
-  require('./result-3-1.jpg'),
-  require('./result-4-1.jpg'),
-  require('./result-5-1.jpg'),
+  require('./result-0-1.png'),
+  require('./result-1-1.png'),
+  require('./result-2-1.png'),
+  require('./result-3-1.png'),
+  require('./result-4-1.png'),
+  require('./result-5-1.png'),
+  require('./result-6-1.png'),
+  require('./result-7-1.png'),
+  require('./result-8-1.png'),
+  require('./result-9-1.png'),
+  require('./result-10-1.png'),
+  require('./result-11-1.png'),
+  require('./result-12-1.png'),
+  require('./result-13-1.png'),
+  require('./result-14-1.png'),
+  require('./result-15-1.png'),
 ];
